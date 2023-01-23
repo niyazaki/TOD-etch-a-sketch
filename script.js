2661261641e745b4afe078677ec19c1012de29e0
Niyazaki <zakkassabeh@hotmail.com>
2023-01-23 02:09:26 +0100

    Added : - on click draw toggle - on hover change bg to purple

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 grid = document.querySelector("#grid");
 grid.style.height = "300px";
 grid.style.width = "300px";
+
+let draw = false;
+document.addEventListener("click", function () {
+  draw = !draw;
+});
+
 function createGrid(squareSize) {
   for (let j = 0; j < squareSize; j++) {
     row = document.createElement("div");
@@ -13,6 +19,7 @@ function createGrid(squareSize) {
       col.id = "col" + i;
       console.log("col" + i);
       col.className = "border-rose-500 border-solid border-2";
+      col.style.backgroundColor = "white";
       //console.log(grid.style);
       console.log(
         grid.style.height,
@@ -24,6 +31,15 @@ function createGrid(squareSize) {
       col.style.height = parseInt(grid.style.height) / squareSize + "px";
       col.style.width = parseInt(grid.style.width) / squareSize + "px";
       console.log(col.style.height, col.style.width);
+
+      col.onmouseover = (function (col) {
+        return function () {
+          if (draw) {
+            col.style.backgroundColor = "indigo";
+          }
+        };
+      })(col);
+
       row.appendChild(col);
       console.log(
         grid.style.height,
